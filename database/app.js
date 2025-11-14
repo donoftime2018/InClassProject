7d62faa5cff58414886d1c5ad1a49930d68a4bf6
donoftime2018 <donoftime2018@gmail.com>
2025-11-13 19:01:34 -0500

    Connection to MongoDB Atlas Database established

--- database/app.js
+++ database/app.js
@@ -1,1 +1,7 @@
-const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://donoftime2018_db_user:I6KZT5YJvdrEDNyM@in-class-cluster.btojmo9.mongodb.net/?appName=in-class-cluster&retryWrites=true").then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+})
