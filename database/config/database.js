685e4f8bee7d39cc4629901b63d1a3dbed32931b
donoftime2018 <donoftime2018@gmail.com>
2025-11-14 17:24:39 -0500

    using atlas uri for database

--- database/config/database.js
+++ database/config/database.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function connectDB() {
-    console.log(process.env.MONGODB_LOCAL);
-    mongoose.connect(process.env.MONGODB_LOCAL).then(() => {
+    console.log(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log('Connected to MongoDB');
     }).catch((error) => {
         console.error('Error connecting to MongoDB:', error);
