a7e41c4759b84b3f0c2550f4bb5ea9cbc9c8dc3e
Dakshesh <your@email.com>
2025-11-14 20:20:31 -0500

    Remove node_modules from repo and ignore it in git

---
+++ ItemCatalog/src/components/login.jsx
@@ -0,0 +1,156 @@
+import {React, useEffect, useState} from "react";
+import {Card, CardContent, Divider, TextField, Button, CardHeader, IconButton} from "@mui/material"
+
+import { useNavigate, Link, useLocation } from "react-router-dom";
+
+import axios from "axios";
+
+import "./Login.css";
+
+//import LoadingIndicator from "../LoadingIndicator/LoadingIndicator";
+
+const LoginPage = (props) => {
+    const [passwordVisibility, setPasswordVisibility] = useState(false)
+    const [loading, setLoading] = useState(false)
+    
+    const showPwd = () => {
+        setPasswordVisibility(true)
+    }
+
+    const hidePwd = () => {
+        setPasswordVisibility(false)
+    }
+
+    useEffect(()=>{
+        document.title = props.title
+    }, [props])
+    
+    const navigate = useNavigate()
+    const location = useLocation()
+    const auth = useAuth()
+
+    const redirect = location.state?.path || '/'
+
+    const validation = () => yup.object({
+        userName: yup.string().required("Username or Email required"),
+        passWord: yup.string().required("Password required")
+    })
+
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues: {
+           userName: "",
+           passWord: ""
+        },
+        validationSchema: validation,
+        onSubmit: (values, actions)=>{
+            checkLogin(values.userName, values.passWord)
+        }
+    }, {})
+
+    const checkLogin = (name, pwd) => {
+        setLoading(true)
+        const data = {name, pwd}
+        const apiEndpoint = process.env.REACT_APP_SERVER_URL + "/login"
+        axios.post(apiEndpoint, data).then((res)=>{
+            if (res.status === 200)
+            {
+                let username = res.data.username
+                auth.login(username)
+                navigate(redirect, {replace: true})
+            }
+        }).catch((err)=>{
+            const errorMessage = JSON.parse(err.request.response);
+            const validationMessage = err.response.data.msg.message;
+            const errorAlert = validationMessage===undefined ? errorMessage.msg : validationMessage;
+            alert(errorAlert);}).finally(()=>{
+                setLoading(false)
+            })
+    }
+
+    return(<>
+        <div class="loginLayout">
+        <Card class="loginCard">
+            <CardHeader sx={{textAlign: 'center'}} title="Login"></CardHeader>
+            <Divider/>
+            <CardContent sx={{display: 'flex', justifyContent: 'center'}}>
+                <form onSubmit={formik.handleSubmit}>
+                    <div>
+                        <TextField
+                            id="userName"
+                            name="userName"
+                            variant="outlined"
+                            type="text"
+                            label="Username or Email"
+                            value={formik.values.userName}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.userName && Boolean(formik.errors.userName)}
+                            helperText={formik.touched.userName && formik.errors.userName}
+                            sx={{ backgroundColor: 'white'}} 
+                            placeholder="Username or Email goes here..." 
+                            disableUnderline="true" 
+                        ></TextField>
+                    </div>
+
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        <TextField
+                            id="passWord"
+                            name="passWord"
+                            variant="outlined"
+                            type={passwordVisibility ? "text" : "password"}
+                            label="Password"
+                            value={formik.values.passWord}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.passWord && Boolean(formik.errors.passWord)}
+                            helperText={formik.touched.passWord && formik.errors.passWord}
+                            sx={{ backgroundColor: 'white'}} 
+                            placeholder="Password goes here..." 
+                            disableUnderline="true" 
+                        ></TextField>
+                        {
+                            isEdge || isEdgeChromium ? 
+                            <></>
+                            :
+                            <>
+                            {
+                                passwordVisibility ? 
+                                <>                                
+                                    <IconButton fontSize="large"><VisibilityIcon onClick={hidePwd}></VisibilityIcon></IconButton>
+                                </> 
+                                : 
+                                <>
+                                    <IconButton fontSize="large"><VisibilityOffIcon onClick={showPwd}></VisibilityOffIcon></IconButton>
+                                </>
+                            }
+                            </>
+                        }
+                    </div>
+
+                    <div style={{display: "flex", justifyContent: 'center'}}>
+                        <Button type="Submit" variant="contained" color="primary" sx={{borderRadius: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Log In</Button>
+                    </div>
+
+            </form>
+            </CardContent>
+            <Divider></Divider>
+            <CardContent style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center'}}>
+                <p><Link to="/register">Click here to sign up.</Link></p>
+                <p><Link to="/forgotPassword">Forgot Password?</Link></p>
+            </CardContent>
+        </Card>
+        </div>
+        
+        {
+            loading ? 
+            <>
+                <LoadingIndicator/>
+            </> 
+            : 
+            <></>
+        }
+    </>)
+}
+
+export default LoginPage;
