f6b0539fd91bd46b06b39fe751d6495b286f0396
donoftime2018 <donoftime2018@gmail.com>
2025-11-14 22:07:00 -0500

    Adding Dashboard as default component (/)

--- ItemCatalog/src/App.jsx
+++ ItemCatalog/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/login'
 import AddItem from './components/addItem'
+import Dashboard from './components/dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,30 +20,7 @@ function App() {
         <Route path="/add-item" element={<AddItem />} />
 
         {/* Home/default page */}
-        <Route path="/" element={
-          <>
-            <div>
-              <a href="https://vite.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-              </button>
-              <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-            </div>
-            <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-            </p>
-          </>
-        } />
+        <Route path="/" element={<Dashboard/> } />
       </Routes>
     </Router>
   )
