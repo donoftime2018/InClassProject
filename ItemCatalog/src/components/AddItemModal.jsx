c5294a42b6d78bdad5455700ab24f6881e055747
donoftime2018 <donoftime2018@gmail.com>
2025-11-20 17:51:44 -0500

    removing return message in axios endpoint in addItemModal

--- ItemCatalog/src/components/AddItemModal.jsx
+++ ItemCatalog/src/components/AddItemModal.jsx
@@ -14,7 +14,7 @@ export default function AddItemModal({ onAdd, onClose }) {
   function handleSubmit(e) {
     e.preventDefault();
       console.log(user.user.username)
-    axios.post(`${import.meta.env.VITE_SERVER_URL}` + "/items/addItem", {name: name, description: desc, price: price, site: link, userName: user.user.username}).then(
+    axios.post(`${import.meta.env.VITE_LOCAL_HOST}` + "/items/addItem", {name: name, description: desc, price: price, site: link, userName: user.user.username}).then(
       (res)=>{
         if (res.status === 200)
         {
@@ -22,7 +22,6 @@ export default function AddItemModal({ onAdd, onClose }) {
         }
     }).catch((err)=>{
         alert(err.response.data.message);
-        return false;
     })
 
   }
